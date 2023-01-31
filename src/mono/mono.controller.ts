b0988b54d210dffef9b623f0caa80305dd8545b7
nkhristyuk <elhe2013@gmail.com>
2023-01-31 21:57:05 +0200

    ...

--- src/mono/mono.controller.ts
+++ src/mono/mono.controller.ts
@@ -34,7 +34,7 @@ export const monoWebHook = async (req: Request, res: Response) => {
     // console.log("webhook body: ",req.body);
     const {invoiceId, status, amount, reference} = req.body
     const invoice: any = await InvoiceModel.findOne({"invoiceId": `${invoiceId}`}).lean();
-    console.log("invoice: ",!!invoice, );
+    // console.log("invoice: ",!!invoice, );
     if (!!invoice ) {
         const config: any = {headers: {Authorization: `Bearer ${postToken}`}}
         const request = {
