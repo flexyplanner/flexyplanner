2864a793290b856b3eed26bc84745b8f0576cb36
nkhristyuk <elhe2013@gmail.com>
2023-03-11 14:18:53 +0200

    ...

--- src/mono/mono.controller.ts
+++ src/mono/mono.controller.ts
@@ -12,8 +12,8 @@ export const monoInvoiceCreate = async (req: Request, res: Response) => {
     if (!!id) {
         const config: any = {
             headers: {
-                'X-Token': 'ugAI3yR-ILBoA2FEZ_C0fZ1l_sERRYPCaL7enjvjHHE8', // тестовый
-                // 'X-Token': 'mXvWdWkZHoTjW4TpK3qFyJw',                         // продакшин
+                // 'X-Token': 'ugAI3yR-ILBoA2FEZ_C0fZ1l_sERRYPCaL7enjvjHHE8', // тестовый
+                'X-Token': 'mXvWdWkZHoTjW4TpK3qFyJw',                         // продакшин
                 'Content-Type': 'application/json; charset=UTF-8'
             }
         }
